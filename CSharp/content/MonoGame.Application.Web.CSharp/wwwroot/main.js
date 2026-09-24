4fb979bb19eade3518e4adaf7695b8cb56827919
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2026-09-24 14:45:21 -0400

    Update template for new structure

--- CSharp/content/MonoGame.Application.Web.CSharp/wwwroot/main.js
+++
@@ -1,7 +0,0 @@
-globalThis.MonoGameWebHostConfiguration = {
-    runtimeScriptUri: "./_framework/dotnet.js",
-    hostExportsTypeName: "MGNamespace.HostExports",
-    mainAssemblyName: "MGNamespace.dll"
-};
-
-await import("./monogame-web-host.js");
